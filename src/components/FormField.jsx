074f4e296711736a10a50f51322da461cdf7bcca
tttttasa <ag20190301@student.fon.bg.ac.rs>
2023-02-07 21:45:13 +0100

    useState hook za email i password input polja

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -4,7 +4,7 @@ function FormField(props) {
     return (
         <div>
             <label>{props.labelText}: &nbsp;</label>
-            <input type={props.type} />
+            <input type={props.type} onChange={(e) => props.funkcija(e.target.value)} />
         </div>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,27 @@
 import FormField from "../components/FormField";
+import { useState } from 'react'
 
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function emailHandle(email) {
+        setEmail(email)
+    }
+
+    function passwordHandle(password) {
+        setPassword(password)
+    }
+
 
     return (
         <div>
             <h1>LOGIN PAGE</h1>
 
-            <FormField type="text" labelText="Email" />
-            <FormField type="password" labelText="Password" />
+            <FormField type="text" labelText="Email" funkcija={emailHandle} />
+            <FormField type="password" labelText="Password" funkcija={passwordHandle} />
 
             <button type="button">Login</button>
 
